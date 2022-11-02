41898c8cd3e412299929bf0c18c32f483e6e8780
whbbit <luckyboy_dong@sina.com>
2022-11-02 23:14:24 +0800

    feat: 环境变量配置自动导入

--- src/router/autoload/index.ts
+++ src/router/autoload/index.ts
@@ -1,42 +1,43 @@
 // 路由自动注册
 // 只支持文件夹下的.vue布局文件，文件夹暂不支持
 
-import { RouteRecordRaw } from "vue-router";
+import { env } from '@/utils/helper'
+import { RouteRecordRaw } from 'vue-router'
 
-const layouts = import.meta.globEager("../../layouts/*.vue");
-const views = import.meta.globEager("../../views/**/*.vue");
+const layouts = import.meta.globEager('../../layouts/*.vue')
+const views = import.meta.globEager('../../views/**/*.vue')
 
 function getRoutes() {
-  const layoutRoutes = [] as RouteRecordRaw[];
+  const layoutRoutes = [] as RouteRecordRaw[]
 
   Object.entries(layouts).forEach(([file, module]) => {
-    const route = getRouteByModule(file, module);
-    route.children = getRouteChildren(route);
-    layoutRoutes.push(route);
-  });
-  return layoutRoutes;
+    const route = getRouteByModule(file, module)
+    route.children = getRouteChildren(route)
+    layoutRoutes.push(route)
+  })
+  return layoutRoutes
 }
 
 function getRouteChildren(layoutRoute: RouteRecordRaw) {
-  const routes = [] as RouteRecordRaw[];
+  const routes = [] as RouteRecordRaw[]
 
   Object.entries(views).forEach(([file, module]) => {
     if (file.includes(`../views/${layoutRoute.name as string}`)) {
-      const route = getRouteByModule(file, module);
-      routes.push(route);
+      const route = getRouteByModule(file, module)
+      routes.push(route)
     }
-  });
-  return routes;
+  })
+  return routes
 }
 
 function getRouteByModule(file: string, module: { [key: string]: any }) {
-  const name = file.replace(/.+layouts\/|.+views\/|\.vue/gi, ""); // 获取layout文件名称
+  const name = file.replace(/.+layouts\/|.+views\/|\.vue/gi, '') // 获取layout文件名称
   const route = {
     path: `/${name}`,
-    name: name.replace("/", "."),
+    name: name.replace('/', '.'),
     component: module.default,
-  } as RouteRecordRaw;
-  return Object.assign(route, module.default?.route);
+  } as RouteRecordRaw
+  return Object.assign(route, module.default?.route)
 }
 
-export default getRoutes();
+export default env.VITE_ROUTE_AUTOLOAD ? getRoutes() : ([] as RouteRecordRaw[])
