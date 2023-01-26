a2a6fbc6ea5fa327a9d2bb7d67265a5f0db6e227
whbbit <luckyboy_dong@sina.com>
2023-01-26 17:13:00 +0800

    feat: 全屏功能

--- src/components/Custom/FullScreen.vue
+++ src/components/Custom/FullScreen.vue
@@ -1,9 +1,25 @@
 <script lang="ts" setup>
-import { ExpandOutline as FullScreen } from '@vicons/ionicons5'
+import { ContractOutline, ExpandOutline } from '@vicons/ionicons5'
+import { ref } from 'vue'
+
+const isFullScreen = ref(false)
+function toggleFullScreen() {
+  isFullScreen.value ? document.exitFullscreen() : document.documentElement.requestFullscreen()
+  isFullScreen.value = !isFullScreen.value
+}
+
+document.addEventListener('fullscreenchange', (e) => {
+  isFullScreen.value = Boolean(document.fullscreenElement)
+})
 </script>
 
 <template>
-  <n-icon size="20" class="duration-300 cursor-pointer hover:scale-110"><FullScreen /></n-icon>
+  <n-icon @click="toggleFullScreen" size="20" class="duration-300 cursor-pointer hover:scale-110">
+    <Transition mode="default">
+      <ExpandOutline v-if="!isFullScreen" />
+      <ContractOutline v-else />
+    </Transition>
+  </n-icon>
 </template>
 
 <style scoped lang="scss"></style>
