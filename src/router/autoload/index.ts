fcd268fafdef61071b59bd7427642d7ab968c839
whbbit <luckyboy_dong@sina.com>
2023-02-19 23:23:20 +0800

    feat: 根据权限动态拼接可访问的路由

--- src/router/autoload/index.ts
+++ src/router/autoload/index.ts
@@ -13,31 +13,38 @@ if (env.VITE_ROUTE_AUTOLOAD) {
   routes = autoloadModuleRoutes()
 }
 
-// todo 根据权限过滤[多级菜单的过滤]
+// 根据权限过滤，动态拼接菜单
 function autoload(router: Router) {
-  const user = userStore()
-  const userInfo = user.userInfo
-  console.log('autoload', userInfo)
   routes = routes.map((route) => {
-    route.children = route.children.filter((r) => {
-      if (r.meta?.permissions?.length) {
-        // 需要权限进行访问
-        return userInfo.permissions.some((permission) => r.meta.permissions.some((p) => p === permission)) ?? false
-      } else {
-        // 不需要权限就能访问
-        return true
-      }
-    })
+    if (route.children) {
+      loopChildren(route.children)
+    }
     return route
   })
+
   routes.forEach((route) => {
     router.addRoute(route)
   })
 }
-export default autoload
 
-// 动态菜单
-async function addDynamicRoutes() {
-  try {
-  } catch (error) {}
+function loopChildren(route: RouteRecordRaw[]) {
+  const user = userStore()
+  const userInfo = user.userInfo
+
+  route.forEach((item) => {
+    if (item.children) {
+      console.log(item.children) // 有子元素就继续往下查找
+      item.children = item.children.filter((r) => {
+        if (r.meta?.permissions?.length) {
+          // 需要权限进行访问
+          return userInfo.permissions.some((permission) => r.meta.permissions.some((p) => p === permission)) ?? false
+        } else {
+          // 不需要权限就能访问
+          return true
+        }
+      })
+      loopChildren(item.children)
+    }
+  })
 }
+export default autoload
