1a0e88d3f1067d5e3468ca801e5f8974fa064077
whbbit <luckyboy_dong@sina.com>
2023-04-24 15:11:53 +0800

    feat: alias报错问题以及去除husky依赖

--- vite/alias.ts
+++ vite/alias.ts
@@ -1,8 +1,8 @@
 import { AliasOptions } from 'vite'
 import path from 'path'
 const alias = {
-  '@': path.resolve(__dirname, '/src'),
-  '#': path.resolve(__dirname, '/types'),
+  '@': path.resolve(__dirname, '../src'),
+  '#': path.resolve(__dirname, '../types'),
 } as AliasOptions
 
 export default alias
