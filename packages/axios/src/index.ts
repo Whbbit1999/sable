227189a2b3260c08523117b502302655e344b07d
whbbit1999 <luckyboy_dong@sina.com>
2024-07-29 17:17:40 +0800

    feat: axios change

--- packages/axios/src/index.ts
+++ packages/axios/src/index.ts
@@ -6,15 +6,26 @@ import type { AxiosInstance, AxiosRequestConfig } from 'axios'
 import type { IAxiosRetryConfig } from 'axios-retry'
 import type { ErrorMessageMode, ResponseResult } from './type'
 
+interface CustomConfig {
+  axiosRetryConfig?: IAxiosRetryConfig
+  authorizationPrefix?: string
+  tokenKey?: string
+}
+
+const defaultConfig: CustomConfig = {
+  axiosRetryConfig: { retries: 3, retryDelay: () => 500 },
+  authorizationPrefix: 'Bearer',
+  tokenKey: 'token',
+}
+
 class Axios {
   private instance: AxiosInstance
   constructor(
     config: AxiosRequestConfig,
-    axiosRetryConfig: IAxiosRetryConfig = { retries: 3, retryDelay: () => 500 },
-    private authorizationPrefix: string = 'Bearer ',
+    public customConfig: CustomConfig = defaultConfig,
   ) {
     this.instance = axios.create(config)
-    this.setupAxiosRetry(axiosRetryConfig)
+    this.setupAxiosRetry(this.customConfig?.axiosRetryConfig || defaultConfig.axiosRetryConfig)
     this.interceptors()
   }
 
@@ -29,7 +40,6 @@ class Axios {
 
   // 请求拦截
   private interceptorsRequest() {
-    // TODO TOKEN自动获取
     this.instance.interceptors.request.use(
       (config) => {
         config.headers.Authorization = this.getToken()
@@ -44,7 +54,6 @@ class Axios {
 
   // 响应拦截
   private interceptorsResponse() {
-    // TODO 响应数据处理
     this.instance.interceptors.response.use(
       (response) => {
         return response
@@ -55,60 +64,11 @@ class Axios {
     )
   }
 
-  private checkStatus(status: number, message: string, errorMessageMode: ErrorMessageMode = 'message') {
-    let errMessage = ''
-    switch (status) {
-      case 400:
-        errMessage = message || '请求错误'
-        break
-      case 401:
-        errMessage = message || '未授权，请登录'
-        break
-      case 403:
-        errMessage = message || '拒绝访问'
-        break
-      case 404:
-        errMessage = message || '请求地址出错'
-        break
-      case 408:
-        errMessage = message || '请求超时'
-        break
-      case 500:
-        errMessage = message || '服务器内部错误'
-        break
-      case 501:
-        errMessage = message || '服务未实现'
-        break
-      case 502:
-        errMessage = message || '网关错误'
-        break
-      case 503:
-        errMessage = message || '服务不可用'
-        break
-      case 504:
-        errMessage = message || '网关超时'
-        break
-      case 505:
-        errMessage = message || 'HTTP版本不受支持'
-        break
-      default:
-        errMessage = message
-        break
-    }
-    if (errMessage) {
-      // TODO: 处理错误信息
-      if (errorMessageMode === 'message') {
-        // message模式
-      }
-      else if (errorMessageMode === 'modal') {
-        // modal模式
-      }
-    }
-  }
-
   private getToken() {
-    const token = localStorage.getItem('token')
-    return token ? `${this.authorizationPrefix}${token}` : ''
+    const { authorizationPrefix, tokenKey } = this.customConfig
+
+    const token = localStorage.getItem(tokenKey)
+    return token ? `${authorizationPrefix} ${token}` : ''
   }
 
   public request<T, D = ResponseResult<T>>(config: AxiosRequestConfig) {
--- src/plugins/axios/index.ts
+++ src/plugins/axios/index.ts
@@ -1,9 +1,13 @@
 import Axios from '@sable/axios'
+import { CacheEnum } from '@/enum/cacheEnums'
 
 const http = new Axios({
   baseURL: '/api',
   timeout: 10000,
   headers: {},
+}, {
+  authorizationPrefix: 'Bearer',
+  tokenKey: CacheEnum.TOKEN,
 })
 
 export default http
