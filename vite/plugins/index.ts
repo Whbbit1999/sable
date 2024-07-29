47430845cac45845f76cc88ab8bb6c5a01b5cde5
whbbit1999 <luckyboy_dong@sina.com>
2024-07-29 17:58:37 +0800

    feat: add vuejsx plugin

--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -2,12 +2,14 @@ import vue from '@vitejs/plugin-vue'
 import { visualizer } from 'rollup-plugin-visualizer'
 import Unocss from 'unocss/vite'
 import vueDevtools from 'vite-plugin-vue-devtools'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import autoImport from './autoImport'
 import setupMockPlugin from './mock'
 
 export default function setupPlugins(isBuild: boolean) {
   return [
     vue(),
+    vueJsx(),
     Unocss(),
     autoImport(),
     setupMockPlugin(isBuild),
