0904e8fa04ec0de3e5444745adab082bdd44b344
whbbit1999 <luckyboy_dong@sina.com>
2024-08-02 18:34:05 +0800

    fix: v-permission

--- src/plugins/directives/permission.ts
+++ src/plugins/directives/permission.ts
@@ -2,7 +2,13 @@ import type { App } from 'vue'
 import { ref, watchEffect } from 'vue'
 import { storeToRefs } from 'pinia'
 
-// 按钮权限
+/**
+ * 根据用户所有权限判断该元素是否展示
+ * v-permission="" 示例：
+ *  - <div v-permission="'admin'"> admin </div>
+ *  - <div v-permission="['admin', 'user']"> user </div>
+ * @param app
+ */
 export function registerPermission(app: App) {
   const { permissions } = storeToRefs(usePermissionStore())
 
@@ -13,29 +19,32 @@ export function registerPermission(app: App) {
       const buttonPermission = Array.isArray(value) ? value : [value]
       const hasPermission = ref(false)
 
-      watchEffect(() => {
-        // 没有获取到权限列表
-        if (!buttonPermission)
-          return
+      // 没有获取到权限列表
+      if (!hasButtonPermission(buttonPermission))
+        return
         // 已经不在DOM树中
-        if (!el.parentNode)
-          return
+      if (!el.parentNode)
+        return
 
-        // 判断是否需要移除该元素
-        hasPermission.value = hasIntersection(permissions, buttonPermission)
+      // 判断是否需要移除该元素
+      hasPermission.value = hasIntersection(permissions, buttonPermission)
 
-        // 没有权限，将元素移除
-        if (!hasPermission.value) {
-          el.parentNode.removeChild(el)
-        }
-      })
+      // 没有权限，将元素移除
+      if (!hasPermission.value) {
+        el.parentNode.removeChild(el)
+      }
     },
 
   })
 }
 
-function hasIntersection(permissions: Ref<string[]>, buttonPermission: string[]) {
+function hasIntersection(permissions: Ref<string[]>, buttonPermission: string[]): boolean {
   const map = new Map()
   permissions.value.forEach(item => map.set(item, true))
   return buttonPermission.some(item => map.has(item))
+}
+
+// 判断元素中是否设置了需要的权限组
+function hasButtonPermission(buttonPermission: string[]): boolean {
+  return buttonPermission.filter(p => Boolean(p)).length > 0
 }
