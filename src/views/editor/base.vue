6150199031cdd3ff0998860fa55e616d661e044d
whbbit1999 <luckyboy_dong@sina.com>
2024-08-11 13:45:15 +0800

    feat: add rich text editor

--- src/views/editor/base.vue
+++ src/views/editor/base.vue
@@ -1,11 +1,34 @@
 <script lang="ts" setup>
+import Quill from '@/components/Editor/Quill.vue'
+
 const content = ref()
 </script>
 
 <template>
   <div>
-    TODO
-    {{ content }}
+    <div class="grid grid-cols-2">
+      <n-card>
+        <template #header>
+          编辑器
+        </template>
+        <div class="min-h-400px">
+          <Quill v-model="content" />
+        </div>
+      </n-card>
+      <n-card>
+        <template #header>
+          预览
+        </template>
+        <div v-html="content" />
+      </n-card>
+    </div>
+
+    <n-card class="mt-4">
+      <template #header>
+        html字符串
+      </template>
+      {{ content }}
+    </n-card>
   </div>
 </template>
 
